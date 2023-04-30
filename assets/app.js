901cdd3885f44eca38dfae0c124358b5b3864d10
Stefan <17656154+snoke@users.noreply.github.com>
2023-04-30 04:10:19 +0200

    improve ui and implement solving

--- assets/app.js
+++ assets/app.js
@@ -10,7 +10,11 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
-
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue';
 import App from './components/App';
 import Base from './components/Base';
--- assets/components/App.vue
+++ assets/components/App.vue
@@ -3,38 +3,83 @@
     <div id="layout " style="height:100vh;">
         <div v-for="(meta,category) in this.questionsData" :key="category">
           <h2>{{meta.category}}</h2>
-          <div v-for="(dataRow,dataRowIndex) in meta.questions" :key="dataRowIndex" style="width:100%;float:left;padding-top:20px;">
-            <h3>
-              {{dataRow.question}}
-            </h3>
-            <div v-for="(answer,answerIndex) in dataRow.answers" :key="answerIndex">
-              <div style="width:100%;float:left;">
-                <input type="checkbox"  style="float:left;" /><span style="float:left;">{{answer.value}}</span>
-              </div>
+          <div class="accordion" role="tablist">
+            <div v-for="(dataRow,dataRowIndex) in meta.questions" :key="dataRowIndex" style="width:100%;float:left;">
+              <b-card no-body class="mb-1">
+                <b-card-header header-tag="header" class="p-1" role="tab">
+                  <b-button v-b-toggle="'collapse-'+category+dataRowIndex" class="m-1" >Toggle Collapse</b-button>
+                </b-card-header>
+                <b-collapse :id="'collapse-'+category+dataRowIndex" visible accordion="my-accordion" role="tabpanel">
+                  <b-card-body>
+                    <b-card-text>
+                      <h3 :ref="'header'+category+'-'+dataRowIndex">
+                        {{dataRow.question}}
+                      </h3>
+                      <div v-for="(answer,answerIndex) in dataRow.answers" :key="answerIndex">
+                        <div style="width:100%;float:left;" v-bind:class="{
+                          'answer-correct':  true===isCorrect(category,dataRowIndex,answerIndex),
+                          'answer-false':  false===isCorrect(category,dataRowIndex,answerIndex),
+                        }">
+                          <input type="checkbox" :ref="'answer-'+category+'-'+dataRowIndex+'-'+answerIndex" style="float:left;margin-top:5px;" /><span style="float:left;"
+                          >
+                          {{answer.value}}</span>
+                        </div>
+                      </div>
+                      <div style="width:100%;float:left;padding-bottom:10px;" >
+                          <b-button variant="success" style="width:100%;" @click="solve(category,dataRowIndex)" v-bind:class="{
+                          'hidden':  true===isSolved(category,dataRowIndex),
+                        }">solve</b-button>
+                      </div>
+                    </b-card-text>
+                  </b-card-body>
+                </b-collapse>
+              </b-card>
             </div>
-              <div style="width:100%;float:left;padding-top:10px;">
-                <input type="button" value="solve" />
-              </div>
           </div>
         </div>
     </div>
 </template>
-
+<style>
+.hidden {
+  display:none;
+}
+.answer-correct {
+  background-color:green;
+}
+.answer-false {
+  background-color:red;
+}
+</style>
 <script>
 export default {
   name: 'App',
   data: function() {
     return {
-        active:null,
+        answers:[],
       }
   },
   methods: {
-      currentRoute(route) {
-       return route==this.$router.currentRoute.name
-      },
-      go(page) {
-        this.$router.push({ name: page})
-      },
+    isSolved(category,dataRowIndex) {
+      return this.answers.hasOwnProperty(category)&&this.answers[category].hasOwnProperty(dataRowIndex)?true:false;
+    },
+    isCorrect(category,dataRowIndex,answerIndex) {
+        if (category in this.answers) {
+          if (dataRowIndex in this.answers[category]) {
+            if (answerIndex in this.answers[category][dataRowIndex]) {
+                return this.answers[category][dataRowIndex][answerIndex];
+            }
+          }
+        }
+    },
+    solve(category,dataRowIndex) {
+      for (let answerIndex in this.questionsData[category]['questions'][dataRowIndex]['answers']) {
+          let correct = this.questionsData[category]['questions'][dataRowIndex]['answers'][answerIndex].correct == this.$refs['answer-'+category+'-'+dataRowIndex+'-'+answerIndex][0].checked;
+          this.answers[category] = this.answers[category]?this.answers[category]:[];
+          this.answers[category][dataRowIndex] = this.answers[category][dataRowIndex]?this.answers[category][dataRowIndex]:[];
+          this.answers[category][dataRowIndex][answerIndex] = correct;
+      }
+      this.$forceUpdate();
+    },
   },
 created: function() {
     this.questionsData = JSON.parse(document.getElementById('_questions').innerHTML);
